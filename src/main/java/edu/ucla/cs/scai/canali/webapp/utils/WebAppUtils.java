152d5ea5313f54ffa42e03a752160dd3bbf40180
lsiciliani <siciliani.lu@gmail.com>
2017-03-08 16:51:45 +0100

    test

--- src/main/java/edu/ucla/cs/scai/canali/webapp/utils/WebAppUtils.java
+++ src/main/java/edu/ucla/cs/scai/canali/webapp/utils/WebAppUtils.java
@@ -1,13 +1,19 @@
 package edu.ucla.cs.scai.canali.webapp.utils;
 
 import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class WebAppUtils {
+//private static final Logger LOGGER = Logger.getLogger( WebAppUtils.class.getName() );
 
 	static public void printParameters(String servletName, HttpServletRequest request) {
+            
 		System.out.println("Print request parameters of " + servletName);
+               // LOGGER.log( Level.SEVERE, "Print request parameters of " + servletName);
+                
 		Enumeration<String> parametersNames = request.getParameterNames();
 		while(parametersNames.hasMoreElements()) {
 			String param = parametersNames.nextElement();
